34c3fd3a6ff84e0d9241219ec33f27423bab23fd
Reena Koudi <reena.koudi@holition.com>
2019-04-10 16:34:52 +0100

    commit

---
+++ src/app/2-todos/todo.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+
+@Injectable()
+export class TodoService {
+  constructor(private http: HttpClient) {
+  }
+
+  add(todo) {
+    return this.http.post('...', todo);
+  }
+
+  getTodos() {
+    return this.http.get<any[]>('...');
+  }
+
+  getTodosPromise() {
+    return this.http.get('...').toPromise();
+  }
+
+  delete(id) {
+    return this.http.delete('...');
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'integration-testing-angular2';
+  title = 'app works!';
 }
